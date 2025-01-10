319faf132b691b7985705908fb6a420e8985b620
Nduati <nduatiquentine@gmail.com>
2025-01-10 17:02:34 +0300

    Adding the deploy.yml file

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,10 @@ import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/fashion-brand/',
+  
   plugins: [react(), svgr()],
    //Github pages hosting configuration
-   
+   base:'/fashion-brand/',
   css: {
    
     preprocessorOptions: {
